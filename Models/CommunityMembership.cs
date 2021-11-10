d5027bf5070070d8b2b9d536f6813bfb783a0d04
25n71w <39124351+25n71w@users.noreply.github.com>
2021-11-09 19:39:45 -0500

    Add files via upload

---
+++ Models/CommunityMembership.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class CommunityMembership
+    {
+        public int StudentID { get; set; }
+
+        public string CommunityID { get; set; }
+
+        
+
+    }
+}
